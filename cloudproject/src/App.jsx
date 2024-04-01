0c58477305e5e3bd1b31db44ba8650398580f626
EvilMarshmallow <matthewlaukw@gmail.com>
2024-04-02 01:14:21 +0800

    form submission is up

--- cloudproject/src/App.jsx
+++ cloudproject/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Dashboard from './Components/Dashboard/Dashboard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Hello World!
+      <div>
+        <Dashboard/>
+      </div>
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
---
+++ cloudproject/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import CoordinateInput from 'react-coordinate-input';
+
+const Dashboard = () => {
+    const [latitude, setLatitude] = useState({
+        latitude: 0
+    });
+    const [longitude, setLongitude] = useState({
+        longitude: 0
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(latitude);
+        console.log(longitude)
+    };
+
+    return (
+        <div>
+            <h1>Welcome to the Dengue Prediction Webapp!</h1>
+            <div> 
+                <h2>Enter your latitude and longitude and let's see how fucked you are!</h2>
+                <form onSubmit={handleSubmit}>
+                    <h3>Round off your inputs to 6 decimal places!!!</h3>
+                        <div>
+                            <label> Latitude: 
+                                    <input type="number" min="0"  step="0.000001" value={latitude} onChange={(event) =>
+
+                                        setLatitude(event.target.value)
+
+                                    }/>                    
+                            </label>                           
+                        </div>
+                        <div>
+                            <label> Longitude: 
+                                    <input type="number" min="0" step="0.000001" value={longitude} onChange={(event) =>
+                                    
+                                        setLongitude(event.target.value)
+                                    
+                                    }/>                    
+                            </label>                       
+                        </div>
+                        <div>
+                            <input type="submit" value="Submit"/>  
+                        </div>
+                </form>
+            </div>
+        </div>
+
+    )
+}
+
+export default Dashboard
