dba1d4194e2c6728450fe35cae6381957a4a3752
JosephHoJJ <89516454+JosephHoJJ@users.noreply.github.com>
2024-04-07 14:04:52 +0800

    update 3

--- src/Table.jsx
+++ src/Table.jsx
@@ -2,6 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import './Table.css';
 
+const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+};
+
 const clearHistory = () => {
   // Clear the cookie by setting its expiration date to a past date
   document.cookie = 'myData=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
